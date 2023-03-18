e021f5c250cefc4b9dfebb7ccf56a0d5e70fefcb
MuchinaWamburia <lucymuchina20@gmail.com>
2023-03-18 13:56:24 +0300

    Migration and seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "🌱 Seeding spices..."
+# restaurants.rb
+
+Restaurant.create(name: "Sottocasa NYC", address: "298 Atlantic Ave, Brooklyn, NY 11201")
+Restaurant.create(name: "PizzArte", address: "69 W 55th St, New York, NY 10019")
+
+
+# pizzas.rb
+
+Pizza.create(name: "Cheese", ingredients: "Dough, Tomato Sauce, Cheese")
+Pizza.create(name: "Pepperoni", ingredients: "Dough, Tomato Sauce, Cheese, Pepperoni")
+
+
+# restaurant_pizzas.rb
+
+RestaurantPizza.create(price: 10.0, restaurant_id: 1, pizza_id: 1)
+RestaurantPizza.create(price: 15.0, restaurant_id: 1, pizza_id: 2)
+RestaurantPizza.create(price: 12.0, restaurant_id: 2, pizza_id: 1)
+
+puts "done"
