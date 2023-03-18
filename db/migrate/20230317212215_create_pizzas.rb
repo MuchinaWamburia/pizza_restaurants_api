10041cd8f0f73f0ae4eb395dbea41b2e9c69860b
MuchinaWamburia <lucymuchina20@gmail.com>
2023-03-18 13:55:59 +0300

    Create migrations

---
+++ db/migrate/20230317212215_create_pizzas.rb
@@ -0,0 +1,11 @@
+class CreatePizzas < ActiveRecord::Migration[7.0]
+  def change
+    create_table :pizzas do |t|
+      t.string :name
+      t.text :ingredients
+      # t.decimal :price
+
+      t.timestamps
+    end
+  end
+end
